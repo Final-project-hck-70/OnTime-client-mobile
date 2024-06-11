81727e79f251d05164a99f00184af14c61879212
zicofachreza <zico.fachreza@gmail.com>
2024-06-11 17:38:41 +0700

    feat: add feature layout report

--- mobile/src/navigators/HomeTab.js
+++ mobile/src/navigators/HomeTab.js
@@ -1,7 +1,14 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../screens/HomeScreen'
-import { Entypo, AntDesign, Octicons, FontAwesome5 } from '@expo/vector-icons'
+import {
+    Entypo,
+    AntDesign,
+    Octicons,
+    FontAwesome5,
+    MaterialIcons,
+} from '@expo/vector-icons'
 import HistoryScreen from '../screens/HistoryScreen'
+import ReportScreen from '../screens/ReportScreen'
 
 const Tab = createBottomTabNavigator()
 
@@ -39,6 +46,29 @@ export default function HomeTab() {
                             />
                         ) : (
                             <Octicons name="history" size={26} color="black" />
+                        ),
+                }}
+            />
+            <Tab.Screen
+                name="Report"
+                component={ReportScreen}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: 'Report',
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused }) =>
+                        focused ? (
+                            <MaterialIcons
+                                name="report"
+                                size={35}
+                                color="black"
+                            />
+                        ) : (
+                            <MaterialIcons
+                                name="report-gmailerrorred"
+                                size={35}
+                                color="black"
+                            />
                         ),
                 }}
             />
---
+++ mobile/src/screens/ReportScreen.js
@@ -0,0 +1,71 @@
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+
+export default function ReportScreen() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}></View>
+            <View style={styles.absoluteContainer}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Report</Text>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.infoCard} activeOpacity={0.6}>
+                <View style={styles.infoCardContent}>
+                    <Text style={styles.infoCardText}>Overtime Submission</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.infoCard} activeOpacity={0.6}>
+                <View style={styles.infoCardContent}>
+                    <Text style={styles.infoCardText}>Leave Submission</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        width: '100%',
+        height: 150,
+        backgroundColor: 'red',
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
+    absoluteContainer: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+    },
+    titleContainer: {
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 70,
+    },
+    title: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: '900',
+        textAlign: 'center',
+    },
+    infoCard: {
+        width: '93%',
+        height: 140,
+        marginTop: 10,
+        backgroundColor: 'black',
+        alignSelf: 'center',
+        borderRadius: 20,
+        justifyContent: 'center',
+    },
+    infoCardContent: {
+        width: '100%',
+    },
+    infoCardText: {
+        fontSize: 20,
+        fontWeight: '600',
+        color: 'white',
+        textAlign: 'center',
+    },
+})
